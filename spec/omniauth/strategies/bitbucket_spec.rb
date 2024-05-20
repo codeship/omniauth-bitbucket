a1da332a5332f7216a2e953f31a2d8819dba67af
Joe Siewert <jsiewert@cloudbees.com>
2024-05-19 21:05:19 -0500

    Test updates

--- spec/omniauth/strategies/bitbucket_spec.rb
+++ spec/omniauth/strategies/bitbucket_spec.rb
@@ -6,27 +6,27 @@ describe OmniAuth::Strategies::Bitbucket do
     let(:strategy) { OmniAuth::Strategies::Bitbucket.new nil }
     
     it 'should fall back to non-primary' do
-      strategy.stub_chain(:access_token, :get) do |url|
+      allow(strategy).to receive_message_chain(:access_token, :get) do |url|
         body = url =~ /emails$/ ? %{{"pagelen": 10, "values": [{"is_primary": true, "is_confirmed": true, "type": "email", "email": "#{email}", "links": {"self": {"href": "https://bitbucket.org/!api/2.0/user/emails/#{email}"}}}], "page": 1, "size": 1}} : '{"user": {}}'
         double body: body
       end
-      strategy.raw_info['email'].should == email
+      expect(strategy.raw_info['email']).to eq(email)
     end
   
     it "should prefer primary" do
-      strategy.stub_chain(:access_token, :get) do |url|
+      allow(strategy).to receive_message_chain(:access_token, :get) do |url|
         body = url =~ /emails$/ ? %{{"pagelen": 10, "values": [{"is_primary": true, "is_confirmed": true, "type": "email", "email": "#{email}", "links": {"self": {"href": "https://bitbucket.org/!api/2.0/user/emails/#{email}"}}}], "page": 1, "size": 1}} : '{"user": {}}'
         double body: body
       end
-      strategy.raw_info['email'].should == email
+      expect(strategy.raw_info['email']).to eq(email)
     end
     
     it "should ignore non-existing" do
-      strategy.stub_chain(:access_token, :get) do |url|
+      allow(strategy).to receive_message_chain(:access_token, :get) do |url|
         body = url =~ /emails$/ ? %{{"pagelen": 0, "values": []}} : '{"user": {}}'
         double body: body
       end
-      strategy.raw_info['email'].should be_nil
+      expect(strategy.raw_info['email']).to eq(nil)
     end
   end
 end
